925d86880139c67aab476a369f0a5ca19785866a
krgh23 <krgh23@naver.com>
2024-12-20 16:23:16 +0900

    20241220

--- push.bat
+++ push.bat
@@ -1,5 +1,12 @@
+@echo off
+
+:: 날짜를 yyyyMMdd 형식으로 가져오기
+for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value ^| find "="') do set datetime=%%I
+set date=%datetime:~0,8%
+
+:: Git 명령어 실행
 git add *
-git commit -m "%date%
+git commit -m "%date%"
 git push origin main
 
 pause
